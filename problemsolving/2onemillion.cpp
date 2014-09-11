f6a8b64ac61c223191f60871911b94b780dbb661
James Sangmok Yun <ysangmok@gmail.com>
2014-09-11 05:19:10 -0500

    folder

---
+++ problemsolving/2onemillion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	//25, 4 = 100
+	//25 25 4 4 
+	int x = 25*25*25;
+	int y = 4*4*4;
+	printf("x = %d\n", x);
+	printf("y = %d\n", y);
+	printf("xy = %d\n",x*y);
+}
